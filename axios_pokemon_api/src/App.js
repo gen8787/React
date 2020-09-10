6b9b075f217c3709a42e59a15e7ed390600c9870
Gary <66178774+gen8787@users.noreply.github.com>
2020-09-10 13:44:44 -0700

    Axios Poke Assignment

---
+++ axios_pokemon_api/src/App.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios' // npm i axios
+
+function App() {
+
+  const [poke, setPoke] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=8') //807
+      .then(res => setPoke(res.data.results))
+      .catch(err => console.log(err))
+  }, []);
+
+  const showPoke = () => {
+    setSuccess(true);
+    console.log(poke);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={showPoke}>Fetch Pokemon</button>
+      {success ? poke.map((poke, idx) => {
+        return <li key={idx}>{poke.name}</li>
+      }) : ''}
+      </div>
+  );
+}
+
+export default App;
